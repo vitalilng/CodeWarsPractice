627ccf0b9f163ef8d85b42905eeca5dd16f3ec36
Vitali Lungu <vitali.lungu@cegeka.com>
2022-02-11 19:19:05 +0200

    new files added

---
+++ LearningCodeWars/Solutions/SumOfNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+/// <summary>
+/// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. 
+/// If the two numbers are equal return a or b.
+/// Note: a and b are not ordered!
+/// </summary>
+
+
+namespace LearningCodeWars.Solutions
+{
+    public class SumOfNumbers
+    {
+        public static int GetSum(int a, int b)
+        {
+            //if (a == b) return a;
+            //if (a > b)
+            //{
+            //    int swap = a;
+            //    a = b;
+            //    b = swap;
+            //}
+
+            //List<int> result = new();
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    result.Add(i);
+            //}
+
+            //return result.Sum();
+
+            return (a + b) * (Math.Abs(a - b) + 1) / 2;
+
+
+        }
+    }
+}
---
+++ LearningCodeWars/Tests/SumOfNumberTests.cs
@@ -0,0 +1,23 @@
+﻿using LearningCodeWars.Solutions;
+using NUnit.Framework;
+
+namespace LearningCodeWars.Tests
+{
+    [TestFixture]
+    public class SumOfNumbersTests
+    {
+        private readonly SumOfNumbers s = new();
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(9, SumOfNumbers.GetSum(-1, 4));
+            Assert.AreEqual(1, SumOfNumbers.GetSum(0, 1));
+            Assert.AreEqual(-1, SumOfNumbers.GetSum(0, -1));
+            Assert.AreEqual(9, SumOfNumbers.GetSum(4, -1));
+            Assert.AreEqual(-3, SumOfNumbers.GetSum(-2, -1));
+            Assert.AreEqual(5, SumOfNumbers.GetSum(-1, 3));
+        }
+
+    }
+}
