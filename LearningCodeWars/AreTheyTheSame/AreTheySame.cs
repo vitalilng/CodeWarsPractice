17cbb13ae5db3b4deceece50061e409aefce5d64
Vitali Lungu <vitali.lungu@cegeka.com>
2022-03-03 13:36:30 +0200

    AreTheySame files updates

--- LearningCodeWars/AreTheyTheSame/AreTheySame.cs
+++ LearningCodeWars/AreTheyTheSame/AreTheySame.cs
@@ -6,58 +6,47 @@ namespace LearningCodeWars.AreTheyTheSame
 {
     public static class AreTheySame
     {
-        public static bool Comp(int[] a, int[] b)
+        public static bool CompareUsingLists(int[] a, int[] b)
         {
             if (a == null || b == null)
             {
                 return false;
             }
 
-            List<int> firstList = a.ToList();
-            List<int> secondList = b.ToList();
-
-            //Console.WriteLine(string.Join(",", firstArray));
-            //Console.WriteLine(string.Join(",", secondArray));
-
-
-            //foreach (var item in firstArray)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            
-
-
+            List<int> firstList = a.OrderBy(i => i).ToList();
+            List<int> secondList = b.OrderBy(i => i).ToList();
 
             for (int i = 0; i < firstList.Count; i++)
             {
                 firstList[i] *= firstList[i];
             }
 
-            int rIndex = 0;
-            for (int i = 0; i < firstList.Count; i++)
-            {
-                for (int j = 0; j < secondList.Count; j++)
-                {
-                    if (firstList[i] == secondList[j])
-                    {
-                        rIndex++;
-                        firstList.RemoveAt(i);
-                        secondList.RemoveAt(j);
-                    }
-                }
-            }
+            return firstList.SequenceEqual(secondList);
+        }
 
-            if (rIndex == firstList.Count)
-            {
-                return true;
-            }
-            else
+        public static bool CompareUsingArrays(int[] a, int[] b)
+        {
+            if (a == null || b == null)
             {
                 return false;
             }
 
+            Array.Sort(a);
+            Array.Sort(b);
 
+            int rIndex = 0;
 
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] *= a[i];
+
+                if (a[i] != b[i])
+                {
+                    rIndex++;
+                }
+            }
+
+            return rIndex == 0;
         }
     }
 }
--- LearningCodeWars/AreTheyTheSame/AreTheySameTests.cs
+++ LearningCodeWars/AreTheyTheSame/AreTheySameTests.cs
@@ -12,7 +12,7 @@ namespace LearningCodeWars.AreTheyTheSame
         {
             int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
             int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
-            bool r = AreTheySame.Comp(a, b); // True
+            bool r = AreTheySame.CompareUsingArrays(a, b); // True
             Assert.AreEqual(true, r);
         }
 
@@ -22,7 +22,7 @@ namespace LearningCodeWars.AreTheyTheSame
         {
             int[] a = new int[] { 2, 2, 3 };
             int[] b = new int[] { 4, 9, 9 };
-            bool r = AreTheySame.Comp(a, b); // True
+            bool r = AreTheySame.CompareUsingArrays(a, b); // True
             Assert.AreEqual(false, r);
         }
 
