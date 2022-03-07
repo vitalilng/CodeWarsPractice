0893d4c09d329071f2507d9c423fb090f48bcec6
Vitali Lungu <vitali.lungu@cegeka.com>
2022-03-07 22:07:11 +0200

    updates to Int32 to Ipv4

--- LearningCodeWars/Int32ToIPv4/Int32ToIpV4.cs
+++ LearningCodeWars/Int32ToIPv4/Int32ToIpV4.cs
@@ -26,6 +26,5 @@ namespace LearningCodeWars.Int23ToIPv4
         {
             return string.Format("{0}.{1}.{2}.{3}", ip >> 24, (ip >> 16) % 256, (ip >> 8) % 256, ip % 256);
         }
-
     }
 }
