ca90325a1a0534e9c975dc35f743fab44fbeabc9
Vitali Lungu <vitali.lungu@cegeka.com>
2022-03-14 23:42:51 +0200

    Sum int in strings

---
+++ LearningCodeWars/SumStringsAsNumbers/SumStrings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+/// <summary>
+/// Solved thanks to https://makolyte.com/algorithm-explained-sum-two-big-integers-the-hard-way/
+/// </summary>
+namespace LearningCodeWars.SumStringsAsNumbers
+{
+    public static class SumStrings
+    {
+        public static string SumString(string a, string b)
+        {
+            //pad a and b with 0's so they are the same length
+            if (a.Length != b.Length)
+            {
+                var maxLength = Math.Max(a.Length, b.Length);
+                a = a.PadLeft(maxLength, '0');
+                b = b.PadLeft(maxLength, '0');
+            }
+
+            var sum = new StringBuilder();
+            int carry = 0;
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                var digitSum = (a[i] - '0') + (b[i] - '0') + carry;
+                if (digitSum > 9)
+                {
+                    carry = 1;
+                    digitSum -= 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                sum.Insert(0, digitSum);
+            }
+
+            if (carry == 1)
+            {
+                sum.Insert(0, carry);
+            }
+
+            return sum.ToString();
+        }
+
+        public static string SumStringUsingTryParse(string a, string b)
+        {
+            BigInteger.TryParse(a, out BigInteger bigStringA);
+            BigInteger.TryParse(b, out BigInteger bigStringB);
+
+            return (bigStringA + bigStringB).ToString();
+        }
+    }
+}
---
+++ LearningCodeWars/SumStringsAsNumbers/SumStringsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LearningCodeWars.SumStringsAsNumbers
+{
+    [TestFixture]
+    public class SumStringsTests
+    {
+        [Test]
+        public void Given123And456Returns579()
+        {
+            Assert.AreEqual("579", SumStrings.SumString("123", "456"));
+            Assert.AreEqual("2147484105", SumStrings.SumString("2147483649", "456"));
+            Assert.AreEqual("5", SumStrings.SumString("5", ""));
+            Assert.AreEqual("712577413488402631964821329", SumStrings.SumString("712569312664357328695151392", "8100824045303269669937"));
+        }
+    }
+}
