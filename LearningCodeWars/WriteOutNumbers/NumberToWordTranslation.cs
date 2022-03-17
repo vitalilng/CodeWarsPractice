67fa10b75b7ae20a493db680e3d55dd39c7d2f6f
Vitali Lungu <vitali.lungu@cegeka.com>
2022-03-17 16:37:06 +0200

    Numbers To Words new files

--- LearningCodeWars/WriteOutNumbers/NumberToWordTranslation.cs
+++ LearningCodeWars/WriteOutNumbers/NumberToWordTranslation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 /// <summary>
 /// Create a function that transforms any positive number to a string
@@ -13,22 +9,80 @@ namespace LearningCodeWars.WriteOutNumbers
 {
     public static class NumberToWordTranslation
     {
+        private static string numberToString = "";
+        private static readonly List<string> ones = new() { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private static readonly List<string> teens = new() { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private static readonly List<string> tens = new() { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+        private const string hundred = " hundred";
+        private const string thousand = " thousand";
+
         public static string NumberToWords(int n)
         {
-            string numberToString = "";
-            string[] tens = { "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-            string[] tenTeen = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "ten" };
-            const string hundred = "hundred";
-            const string thousand = "thousand";
-            
-            
-            
-            
-
-
+            if (n >= 0 && n < 10)
+            {
+                GetTheOnes(n);
+            }
+            else if (n > 9 && n < 20)
+            {
+                GetTheTeens(n);
+            }
+            else if (n > 19 && n < 100)
+            {
+                GetTheTens(n);
+            }
+            else if (n > 99 && n < 1000)
+            {
+                GetTheHundreds(n);
+            }
+            else if (n > 999 && n < 1000000)
+            {
+                GetTheThousands(n);
+            }
 
             return numberToString;
+        }
 
+        public static string GetTheOnes(int n)
+        {
+            numberToString = ones[n];
+            return numberToString;
+        }
+
+        public static string GetTheTeens(int n)
+        {
+            numberToString = teens[n - 10];
+            return numberToString;
+        }
+
+        public static string GetTheTens(int n)
+        {
+            numberToString = tens[n / 10] + ((n % 10 != 0) ? "-" + GetTheOnes(n % 10) : "");
+            return numberToString;
+        }
+
+        public static string GetTheHundreds(int n)
+        {
+            if (n % 100 != 0 && n % 100 < 10)
+            {
+                numberToString = GetTheOnes(n / 100) + hundred + " " + GetTheOnes(n % 100);
+            }
+            else if (n % 100 != 0 && n % 100 > 10 && n % 100 < 20)
+            {
+                numberToString = GetTheOnes(n / 100) + hundred + " " + GetTheTeens(n % 100);
+            }
+            else
+            {
+                numberToString = GetTheOnes(n / 100) + hundred + ((n % 100 != 0) ? " " + NumberToWords(n % 100) : "");
+            }
+
+            return numberToString;
+        }
+
+        public static string GetTheThousands(int n)
+        {
+            numberToString = NumberToWords(n / 1000) + thousand + ((n % 1000 != 0) ? " " + NumberToWords(n % 1000) : "");
+            return numberToString;
         }
     }
 }
--- LearningCodeWars/WriteOutNumbers/NumberToWordTranslationTests.cs
+++ LearningCodeWars/WriteOutNumbers/NumberToWordTranslationTests.cs
@@ -8,18 +8,21 @@ namespace LearningCodeWars.WriteOutNumbers
         [Test]
         public void Number2WordsTest()
         {
-
             Assert.AreEqual("zero", NumberToWordTranslation.NumberToWords(0));
             Assert.AreEqual("one", NumberToWordTranslation.NumberToWords(1));
             Assert.AreEqual("three", NumberToWordTranslation.NumberToWords(3));
             Assert.AreEqual("five", NumberToWordTranslation.NumberToWords(5));
             Assert.AreEqual("eight", NumberToWordTranslation.NumberToWords(8));
+            Assert.AreEqual("ten", NumberToWordTranslation.NumberToWords(10));
+            Assert.AreEqual("eleven", NumberToWordTranslation.NumberToWords(11));
             Assert.AreEqual("three hundred one", NumberToWordTranslation.NumberToWords(301));
             Assert.AreEqual("seven hundred ninety-three", NumberToWordTranslation.NumberToWords(793));
             Assert.AreEqual("eight hundred", NumberToWordTranslation.NumberToWords(800));
             Assert.AreEqual("six hundred fifty", NumberToWordTranslation.NumberToWords(650));
             Assert.AreEqual("one thousand", NumberToWordTranslation.NumberToWords(1000));
             Assert.AreEqual("one thousand three", NumberToWordTranslation.NumberToWords(1003));
+            Assert.AreEqual("eleven thousand thirteen", NumberToWordTranslation.NumberToWords(11013));
+            Assert.AreEqual("nine hundred ninety-nine thousand nine hundred ninety-nine", NumberToWordTranslation.NumberToWords(999999));
         }
     }
 }
